6a2adb23934268c2e4840e246dbff4b5476e4221
Matt Jackels <matt.jackels@gmail.com>
2018-02-04 14:49:11 -0600

    Added profile list/create feature for user profile api app

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from . import serializers
+from . import models
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -106,3 +107,9 @@ class HelloViewSet(viewsets.ViewSet):
         '''removes an object'''
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    '''handles creating and updating profiles'''
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
